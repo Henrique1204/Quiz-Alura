ccd1ea5101377250d0eed5d225850a93d4d9d83e
Paulo <pauloh16.pdr@gmail.com>
2021-01-26 14:35:42 -0300

    Implementar next/head

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import db from "../db.json";
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+// eslint-disable-next-line import/no-unresolved
+import Head from 'next/head';
+// Importando configurações do projeto.
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
  * {
@@ -23,17 +27,27 @@ const GlobalStyle = createGlobalStyle`
     flex: 1;
     display: flex;
     flex-direction: column;
-  }	 
+  }
 `;
 
+const { theme } = db;
 
-const theme = db.theme;
-
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta property="og:image" content={db.bg} />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <title>Quiz Pokemon</title>
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
-};
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 // Importando componentes da interface.
-import Widget from "../src/Componentes/Widget";
-import QuizBackground from "../src/Componentes/QuizBackground";
-import QuizLogo from "../src/Componentes/QuizLogo";
-import Footer from "../src/Componentes/Footer";
-import GithubCorner from "../src/Componentes/GithubCorner";
+import Widget from '../src/Componentes/Widget';
+import QuizBackground from '../src/Componentes/QuizBackground';
+import QuizLogo from '../src/Componentes/QuizLogo';
+import Footer from '../src/Componentes/Footer';
+import GithubCorner from '../src/Componentes/GithubCorner';
 // Importando informações do projeto.
-import db from "../db.json";
+import db from '../db.json';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -32,15 +33,15 @@ export default function Home() {
           </Widget.Header>
 
           <Widget.Content>
-              <p>{db.description}</p>
+            <p>{db.description}</p>
           </Widget.Content>
         </Widget>
-  
+
         <Widget>
           <Widget.Header>
             <h1>Quiz da Galera</h1>
           </Widget.Header>
-    
+
           <Widget.Content>
             <p>Lorem impsum door set amet...</p>
           </Widget.Content>
